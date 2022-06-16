effaab00b932144bec7cb561299f8fd1c1fa2611
cholas <zyw598@126.com>
2022-06-16 17:26:38 +0800

    完成问题详情页面

--- src/main/java/com/cholas/community/service/QuestionService.java
+++ src/main/java/com/cholas/community/service/QuestionService.java
@@ -94,4 +94,13 @@ public class QuestionService {
         paginationDTO.setQuestions(questionDTOList);
         return paginationDTO;
     }
+
+    public QuestionDTO getById(Integer id) {
+        Question question = questionMapper.getById(id);
+        User user = userMapper.findById(question.getCreator());
+        QuestionDTO questionDTO = new QuestionDTO();
+        BeanUtils.copyProperties(question, questionDTO);
+        questionDTO.setUser(user);
+        return questionDTO;
+    }
 }
